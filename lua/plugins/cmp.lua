ea4f3979437a7d371e1289ec0cc54a36691d0d55
simenvl <lsimen89@gmail.com>
2023-01-19 14:04:28 +0100

    ✨ add cmp and bufferline

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,106 +1,106 @@
 local M = {
- "hrsh7th/nvim-cmp",
- event = "InsertEnter",
- dependencies = {
-  "hrsh7th/cmp-buffer",
-  "hrsh7th/cmp-nvim-lsp",
-  "hrsh7th/cmp-path",
-  "hrsh7th/cmp-nvim-lua",
-  "L3MON4D3/LuaSnip",
-  "onsails/lspkind-nvim",
-  { "hrsh7th/cmp-cmdline", enabled = cmdline },
-    { "dmitmel/cmp-cmdline-history", enabled = cmdline },
- },
+	"hrsh7th/nvim-cmp",
+	event = "InsertEnter",
+	dependencies = {
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-nvim-lua",
+		"L3MON4D3/LuaSnip",
+		"onsails/lspkind-nvim",
+		{ "hrsh7th/cmp-cmdline", enabled = cmdline },
+		{ "dmitmel/cmp-cmdline-history", enabled = cmdline },
+	},
 }
 
 function M.config()
- local cmp = require("cmp")
- local lspkind = require("lspkind")
- cmp.setup({
-   	snippet = {
-		expand = function(args)
-			luasnip.lsp_expand(args.body)
-		end,
-	},
-	mapping = cmp.mapping.preset.insert({
-		["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
-		["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
-		["<C-b>"] = cmp.mapping.scroll_docs(-4),
-		["<C-f>"] = cmp.mapping.scroll_docs(4),
-		["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
-		["<C-e>"] = cmp.mapping.abort(), -- close completion window
-		["<CR>"] = cmp.mapping.confirm({ select = false }),
+	local cmp = require("cmp")
+	local lspkind = require("lspkind")
+	cmp.setup({
+		snippet = {
+			expand = function(args)
+				luasnip.lsp_expand(args.body)
+			end,
+		},
+		mapping = cmp.mapping.preset.insert({
+			["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
+			["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
+			["<C-b>"] = cmp.mapping.scroll_docs(-4),
+			["<C-f>"] = cmp.mapping.scroll_docs(4),
+			["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
+			["<C-e>"] = cmp.mapping.abort(), -- close completion window
+			["<CR>"] = cmp.mapping.confirm({ select = false }),
 
-		["<Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_next_item()
-			elseif luasnip.expandable() then
-				luasnip.expand()
-			elseif luasnip.expand_or_jumpable() then
-				luasnip.expand_or_jump()
-			elseif check_backspace() then
-				fallback()
-			else
-				fallback()
-			end
-		end, {
-			"i",
-			"s",
+			["<Tab>"] = cmp.mapping(function(fallback)
+				if cmp.visible() then
+					cmp.select_next_item()
+				elseif luasnip.expandable() then
+					luasnip.expand()
+				elseif luasnip.expand_or_jumpable() then
+					luasnip.expand_or_jump()
+				elseif check_backspace() then
+					fallback()
+				else
+					fallback()
+				end
+			end, {
+				"i",
+				"s",
+			}),
+			["<S-Tab>"] = cmp.mapping(function(fallback)
+				if cmp.visible() then
+					cmp.select_prev_item()
+				elseif luasnip.jumpable(-1) then
+					luasnip.jump(-1)
+				else
+					fallback()
+				end
+			end, {
+				"i",
+				"s",
+			}),
 		}),
-		["<S-Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_prev_item()
-			elseif luasnip.jumpable(-1) then
-				luasnip.jump(-1)
+		enabled = function()
+			-- disable completion in comments
+			local context = require("cmp.config.context")
+			-- keep command mode completion enabled when cursor is in a comment
+			if vim.api.nvim_get_mode().mode == "c" then
+				return true
 			else
-				fallback()
+				return not context.in_treesitter_capture("comment") and not context.in_syntax_group("Comment")
 			end
-		end, {
-			"i",
-			"s",
+		end,
+		-- sources for autocompletion
+		sources = cmp.config.sources({
+			{ name = "nvim_lsp" },
+			{ name = "luasnip" },
+			{ name = "buffer" },
+			{ name = "nvim_lua" },
+			{ name = "path" },
 		}),
-	}),
-	enabled = function()
-		-- disable completion in comments
-		local context = require("cmp.config.context")
-		-- keep command mode completion enabled when cursor is in a comment
-		if vim.api.nvim_get_mode().mode == "c" then
-			return true
-		else
-			return not context.in_treesitter_capture("comment") and not context.in_syntax_group("Comment")
-		end
-	end,
-	-- sources for autocompletion
-	sources = cmp.config.sources({
-		{ name = "nvim_lsp" },
-		{ name = "luasnip" },
-		{ name = "buffer" },
-		{ name = "nvim_lua" },
-		{ name = "path" },
-	}),
-	-- configure lspkind for vs-code like icons
-	formatting = {
-		format = lspkind.cmp_format({ with_text = false, maxwidth = 50 }),
-	},
-	confirm_opts = {
-		behavior = cmp.ConfirmBehavior.Replace,
-		select = false,
-	},
-	experimental = {
-		ghost_text = false,
-	},
- })
-if cmdline then
-    cmp.setup.cmdline(":", {
-      mapping = cmp.mapping.preset.cmdline(),
-      sources = cmp.config.sources({
-        -- { name = "noice_popupmenu" },
-        { name = "path" },
-        { name = "cmdline" },
-        -- { name = "cmdline_history" },
-      }),
-    })
-  end
+		-- configure lspkind for vs-code like icons
+		formatting = {
+			format = lspkind.cmp_format({ with_text = false, maxwidth = 50 }),
+		},
+		confirm_opts = {
+			behavior = cmp.ConfirmBehavior.Replace,
+			select = false,
+		},
+		experimental = {
+			ghost_text = false,
+		},
+	})
+	if cmdline then
+		cmp.setup.cmdline(":", {
+			mapping = cmp.mapping.preset.cmdline(),
+			sources = cmp.config.sources({
+				-- { name = "noice_popupmenu" },
+				{ name = "path" },
+				{ name = "cmdline" },
+				-- { name = "cmdline_history" },
+			}),
+		})
+	end
 end
 
 return M
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -13,4 +13,36 @@ return {
 	"kylechui/nvim-surround",
 	"aserowy/tmux.nvim",
 	{ "danymat/neogen", requires = "nvim-treesitter/nvim-treesitter" },
+
+	{
+		"windwp/nvim-autopairs",
+		config = function()
+			require("nvim-autopairs").setup({
+				check_ts = true, -- treesitter integration
+				disable_filetype = { "TelescopePrompt" },
+				ts_config = {
+					lua = { "string", "source" },
+					javascript = { "string", "template_string" },
+					java = false,
+				},
+
+				fast_wrap = {
+					map = "<M-e>",
+					chars = { "{", "[", "(", '"', "'" },
+					pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
+					offset = 0, -- Offset from pattern match
+					end_key = "$",
+					keys = "qwertyuiopzxcvbnmasdfghjkl",
+					check_comma = true,
+					highlight = "PmenuSel",
+					highlight_grey = "LineNr",
+				},
+			})
+
+			local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+			local cmp = require("cmp")
+
+			cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+		end,
+	},
 }
